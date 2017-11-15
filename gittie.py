52bde358fec0ecd824046d0c16103776568ca64e
Oláh Tamás <tolah94@gmail.com>
2017-11-15 09:55:50 +0100

    Modified introduce() function

--- gittie.py
+++ gittie.py
@@ -1,15 +1,13 @@
 def introduce():
-    print("Hello, I'm Gittie!")
+    print("Heya!")
+    print("I'm Gittie! Nice to meet you!")
 
 
-<<<<<<< HEAD
 def add(a, b):
     return a  + b
 
 
-=======
 def joke():
     print("Can a kangaroo jump higher than a house?")
     print("-")
     print("Of course, a house doesn’t jump at all.")
->>>>>>> 5c612e2da8587c19b31ea743225381a69b2844e1
