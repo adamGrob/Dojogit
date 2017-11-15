f40d85756a882d4fb9734b4172b952fe7e7932b8
Adam Grob <ad4mgr0b@gmail.com>
2017-11-15 09:55:35 +0100

    added some changes to the introduce() function

--- gittie.py
+++ gittie.py
@@ -1,15 +1,17 @@
 def introduce():
-    print("Hello, I'm Gittie!")
+    name_list = ["Adam", "Thomas"]
+    for item in name_list:
+        print("Hello, I'm {}".format(item))
 
 
-<<<<<<< HEAD
 def add(a, b):
     return a  + b
 
 
-=======
 def joke():
     print("Can a kangaroo jump higher than a house?")
     print("-")
     print("Of course, a house doesn’t jump at all.")
->>>>>>> 5c612e2da8587c19b31ea743225381a69b2844e1
+
+
+introduce()
