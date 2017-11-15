5c612e2da8587c19b31ea743225381a69b2844e1
Oláh Tamás <tolah94@gmail.com>
2017-11-15 09:40:41 +0100

    Added joke() function

--- gittie.py
+++ gittie.py
@@ -1,2 +1,8 @@
 def introduce():
     print("Hello, I'm Gittie!")
+
+
+def joke():
+    print("Can a kangaroo jump higher than a house?")
+    print("-")
+    print("Of course, a house doesn’t jump at all.")
